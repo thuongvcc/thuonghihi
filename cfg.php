8cf81f88889822084d4fb43bde3fbb69f47422e2
thuongvcc <35647119+thuongvcc@users.noreply.github.com>
2019-12-01 09:15:39 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$device = "android";
+$deviceId = "08a23ed035482b68";
+$idfa = "bd1b7886-a5bc-40c8-b2c6-c847ad2b86b6";
+$info_tel = "Xiaomi Redmi 4A";
+$userId = "5dc2fe7acccb4851f7896c59";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
