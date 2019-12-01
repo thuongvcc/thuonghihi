3cacc03265244ce6321a3a6163b111b1cd068fe5
thuongvcc <35647119+thuongvcc@users.noreply.github.com>
2019-12-01 09:17:52 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -2,10 +2,10 @@
 
 
 $device = "android";
-$deviceId = "08a23ed035482b68";
-$idfa = "bd1b7886-a5bc-40c8-b2c6-c847ad2b86b6";
-$info_tel = "Xiaomi Redmi 4A";
-$userId = "5dc2fe7acccb4851f7896c59";
+$deviceId = "cd3f729c6950ded8";
+$idfa = "idfa":"2094b7c3-5d1b-43d0-aa1c-66f6b3927cb2";
+$info_tel = "OPPO CPH1825";
+$userId = "5de31768bab4a87efa367839";
 
 
 
